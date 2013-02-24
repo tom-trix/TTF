c04256db1e398fa9101865789f5eddde0aa1b05e
tom-trix_nbw <tom-trix@ya.ru>
2013-02-25 02:11:09 +0600

    Absolutely new version of ExtendedSearch

--- src/main/scala/ru/tomtrix/ttf/Enums.scala
+++ src/main/scala/ru/tomtrix/ttf/Enums.scala
@@ -7,3 +7,9 @@ abstract sealed class DBMS
 
 object SQLITE extends DBMS
 object MYSQL extends DBMS
+
+object Keyboard {
+  val BACKSPACE = 8
+  val ENTER = 13
+  val DELETE = 127
+}
--- src/main/scala/ru/tomtrix/ttf/ExtendedSearch.scala
+++ src/main/scala/ru/tomtrix/ttf/ExtendedSearch.scala
@@ -1,17 +1,12 @@
 package ru.tomtrix.ttf
 
-import SWTWrappers._
-import akka.actor.Actor
-import patterns.ActorsManager
+import scala.List
+import collection.mutable.ArrayBuffer
 import org.eclipse.swt.events._
 import org.eclipse.swt.{SWT, widgets}
 import org.eclipse.swt.layout.FillLayout
+import org.eclipse.swt.graphics.{Image, GC}
 import widgets._
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits._
-import collection.mutable.ArrayBuffer
-import scala.List
-import org.eclipse.swt.graphics.{Image, Color, GC}
 
 
 /**
@@ -20,20 +15,16 @@ import org.eclipse.swt.graphics.{Image, Color, GC}
 class ExtendedSearch(tbox: Text) {
   private val shell = new Shell(Display getDefault, SWT.RESIZE | SWT.ON_TOP)
   private val list = new widgets.List(shell, SWT.SINGLE | SWT.BORDER)
-  private val sourceShell = tbox getShell()
   private val data = ArrayBuffer[String]()
-  val img = new Image(Display getDefault, tbox.getBounds)
+  val img = new Image(Display getDefault, tbox getBounds)
   val gc = new GC(img)
-  val hide = () => shell setVisible false
-  val accept = () => {
-    tbox.setText(list.getSelection.apply(0))
-    hide()
+  val hide = () => {
+    shell setVisible false
+    tbox setBackgroundImage null
   }
-  val moveSelection = (t: Int) => {
-    shell setActive()
-    val i = list.getSelectionIndex+t
-    if (0 <= i && i < list.getItemCount)
-      list setSelection i
+  val accept = () => {
+    tbox setText list.getSelection.apply(0)
+    hide()
   }
   val drawCaretStamp = () => {
     gc fillRectangle img.getBounds
@@ -41,95 +32,67 @@ class ExtendedSearch(tbox: Text) {
     tbox setBackgroundImage null
     tbox setBackgroundImage img
   }
-  shell.setLayout(new FillLayout())
+  shell setLayout new FillLayout
   shell setSize(200, 100)
 
-  tbox.addModifyListener(new ModifyListener {
+  tbox addModifyListener(new ModifyListener {
     def modifyText(e: ModifyEvent) {
-      shell setLocation(tbox.toDisplay(0,0).x, tbox.toDisplay(0,0).y + tbox.getSize.y + 5)
       if (!shell.isVisible) {
-        shell.open()
-        sourceShell.setActive()
+        shell setLocation(tbox.toDisplay(0, 0).x, tbox.toDisplay(0, 0).y + tbox.getSize.y + 5)
+        shell open()
       }
     }
   })
 
-  tbox.addKeyListener(new KeyAdapter {
+  list addKeyListener(new KeyAdapter {
     override def keyPressed(e: KeyEvent) {
-      if (shell.isVisible)
-        e.keyCode match {
-          case SWT.ARROW_DOWN => {
-            moveSelection(1)
-            e.doit = false
-          }
-          case SWT.ARROW_UP => {
-            moveSelection(-1)
-            e.doit = false
-          }
-          case SWT.ESC => hide()
-          case _ =>
-        }
-    }
-  })
-
-  list.addKeyListener(new KeyAdapter {
-    override def keyPressed(e: KeyEvent) {
+      val pos = tbox getCaretPosition()
       e.keyCode match {
+        case Keyboard.BACKSPACE => {
+          tbox setSelection(pos-1, pos)
+          tbox cut()
+          drawCaretStamp()
+        }
+        case Keyboard.DELETE => {
+          tbox setSelection(pos, pos+1)
+          tbox cut()
+        }
+        case Keyboard.ENTER => accept()
         case SWT.SPACE => accept()
-        case 13 => accept()
         case SWT.ESC => hide()
         case SWT.ARROW_LEFT => {
-          tbox.setSelection(tbox.getCaretPosition-1)
+          tbox setSelection pos-1
           drawCaretStamp()
           e.doit = false
         }
         case SWT.ARROW_RIGHT => {
-          tbox.setSelection(tbox.getCaretPosition+1)
+          tbox setSelection pos+1
           drawCaretStamp()
           e.doit = false
         }
         case _ => if (!e.character.toString.trim.isEmpty) {
-          tbox insert(e.character.toString)
+          tbox insert e.character.toString
           drawCaretStamp()
+          e.doit = false
         }
       }
     }
   })
 
-  list.addMouseListener(new MouseAdapter {
+  list addMouseListener(new MouseAdapter {
     override def mouseDoubleClick(e: MouseEvent) {
       accept()
     }
   })
 
-  sourceShell.addShellListener(new ShellAdapter {
-    override def shellIconified(e: ShellEvent) {
-      hide()
-    }
-  })
-
-  (sourceShell :: sourceShell.getChildren.toList) foreach {t => t.addMouseListener(new MouseAdapter {
-    override def mouseDown(e: MouseEvent) {
-      hide()
-    }
-  })}
-
-  shell.addShellListener(new ShellAdapter {
+  shell addShellListener(new ShellAdapter {
     override def shellActivated(e: ShellEvent) {
       drawCaretStamp()
     }
     override def shellDeactivated(e: ShellEvent) {
-      tbox setBackgroundImage null
+      hide()
     }
   })
-
-  ActorsManager.system.scheduler.schedule(0 minutes, 127 milliseconds) {
-    invokeAsynch {
-      val curShell = Display.getDefault.getActiveShell
-      if (curShell != shell && curShell != sourceShell && !shell.isDisposed)
-        hide()
-    }
-  }
 
   def addData(searchedData: List[String]) {
     data ++= searchedData
@@ -139,6 +102,9 @@ class ExtendedSearch(tbox: Text) {
   }
 }
 
+/**
+ * grgsd
+ */
 object ExtendedSearch {
   implicit def toExtendedSearch(source: Text) =
     new ExtendedSearch(source)
